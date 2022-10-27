666256be8dad6e2f175beb6577a362956e64638b
Marcus <hymanmarcus95@gmail.com>
2022-10-27 15:33:33 -0400

    add text to Landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,10 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Hello, {user ? user.name : 'Summoner.'}</h1>
+      <h1>Welcome to Gathering Magic. Here you will be able to build your very own deck for Magic The Gathering.</h1>
+      <h2> If you are new here please click the "Sign up" button.</h2> 
+      <h2>If you already have an account click "Log In."</h2>
     </main>
   )
 }
