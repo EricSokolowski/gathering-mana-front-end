d547b4e6c50b52c3599457a57adbe6d46b8dda83
davidv <davidvazquezgit@gmail.com>
2022-10-27 11:24:22 -0700

    rearrange nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,19 +5,27 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <nav>
       {user ?
-        <ul>
+        <ul className="link">
           Welcome, {user.name}
-          <Link to="/profiles">Profiles</Link>
-          <Link to="" onClick={handleLogout}>LOG OUT</Link>
-          <Link to="/changePassword">Change Password</Link>
-          <Link to="/new-deck">Build Deck</Link>
-          <Link to="/decks-index">Deck Page</Link>
+          <Link to="/profiles" 
+            style={{ textDecoration: 'none' }}>Profiles</Link>
+          <Link to="/decks-index" 
+            style={{ textDecoration: 'none'}}>Deck Page</Link>
+          <Link to="/new-deck" 
+            style={{ textDecoration: 'none' }}>Build Deck</Link>
+          <Link to="/changePassword" 
+            style={{ textDecoration: 'none' }}>Change Password</Link>
+          <Link to="" onClick={handleLogout} 
+            style={{ textDecoration: 'none' }}>LOG OUT</Link>
         </ul>
         :
-        <ul>
-          <Link to="/decks-index">Deck Page</Link>
-          <Link to="/login">Log In</Link>
-          <Link to="/signup">Sign Up</Link>
+        <ul className="link">
+          <Link to="/decks-index" 
+          style={{ textDecoration: 'none' }}>Deck Page</Link>
+          <Link to="/login" 
+          style={{ textDecoration: 'none' }}>Log In</Link>
+          <Link to="/signup" 
+          style={{ textDecoration: 'none' }}>Sign Up</Link>
         </ul>
       }
     </nav>
