3fc8c45350d3a1fd53f609257a86fa08128b5271
EricSoklowski <ericsoko2002@gmail.com>
2022-10-27 08:19:47 -0400

    edit deckCard

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = ({ user, handleLogout }) => {
           <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
           <li><Link to="/changePassword">Change Password</Link></li>
           <li><Link to="/new-deck">Build Deck</Link></li>
-          <li><Link to="/decks-index">Deck Page</Link></li>
+          <li><Link to="/decks-index">View Decks</Link></li>
         </ul>
         :
         <ul>
