c8ea79203dbb8198d9296472c22d4e349c19e856
davidv <davidvazquezgit@gmail.com>
2022-11-01 14:39:30 -0700

    add styling to landind, login and sign up page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,14 +10,18 @@ const LoginPage = props => {
   }
 
   return (
-    <main className={styles.container}>
-      <h1>Log In</h1>
-      <p>{message}</p>
-      <LoginForm
-        handleSignupOrLogin={props.handleSignupOrLogin}
-        updateMessage={updateMessage}
-      />
-    </main>
+    <section  className={styles.loginImg}>
+      <main className={styles.container} >
+        <section>
+          <h1>Log In</h1>
+          <p>{message}</p>
+          <LoginForm
+            handleSignupOrLogin={props.handleSignupOrLogin}
+            updateMessage={updateMessage}
+          />
+        </section>
+      </main>
+    </section >
   )
 }
 
